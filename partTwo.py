71b422bf7b347112c811d44f29bed8cf293a49bf
eb582 <eb582@kent.ac.uk>
2024-10-02 11:32:38 +0000

    bim bash bosh

--- partTwo.py
+++ partTwo.py
@@ -1,9 +1,12 @@
 import math  
 
 def main():
-#TO DO  
+    A = int(input("what's the length of A? "))
+    B = int(input("what's the length of B? "))
+    C = pythag(A,B)
+    print("The Length of C is = " , C)
 
 def pythag(A,B):
-#TO DO  
-
+    return math.sqrt(A ** 2 + B ** 2)
+    
 main()
